d682c9da02c3dfacbd06428cfef5c1289009d92d
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-01 17:36:21 -0300

    idea now updates properly

--- bot/commands/ideas/idea.ts
+++ bot/commands/ideas/idea.ts
@@ -1,33 +1,63 @@
-const { setIdea } = require("../../functions/ideas/ideas");
+const { addIdea, getIdea } = require("../../functions/ideas/ideas");
 
 const userCache = require("../../data/cache/users.json");
+let ideaCache = require("../../data/cache/ideas.json");
+
+const idea = {
+    title: '',
+    discutted: {
+        at: null,
+        by: [],
+        for: null
+    },
+    created: {
+        at: new Date(),
+        by: ''
+    },
+    development: [
+    ]
+};
+
+
+async function ideaList(msg, args) {
+    if (args[0] != 'list') return
+
+    var ideas = userCache[msg.author.id].ideas
+
+    if (args[1] == 'all') {
+    }
+
+    await Promise.all(ideas.map(async (ideaId) => await getIdea(ideaId)));
+
+    let reply = `Sua lista de ideias:\n`;
+
+    ideas.forEach((ideaId) => {
+        const title = ideaCache[ideaId]?.title || 'Ideia não encontrada :(';
+        reply += `${ideaId}: ${title}\n`;
+    });
+
+    msg.reply(reply)
+}
+
+async function ideaAdd(msg, args) {
+    if (args[0] == 'list' && args.length == 1) return
+    idea.title = args.join(' ');
+    idea.created.by = msg.author.id
+
+    const ideaId = await addIdea(idea);
+
+    userCache[msg.author.id].ideas.push(ideaId);
+    await msg.reply(`A ideia **${idea.title}** foi adicionada a sua lista de ideias com o id **${ideaId}**, para debatê-la execute o comando **;discutir ${ideaId}**`);
+}
 
 module.exports = {
     name: "ideia",
     aliases: ['idea', 'i'],
     description: ("Adiciona ideias a sua lista de ideias"),
     args: true,
-    usage: '<ideaTitle>',
+    usage: '<ideaTitle> | <ideaId> | list <all>',
     async execute(msg, args) {
-        const ideaTitle = args.join(' ')
-        const idea = {
-            title: ideaTitle,
-            discutted:{
-                at: null,
-                by: [],
-                for: null
-            },
-            created: {
-                at: new Date(),
-                by: msg.author.id
-            },
-            development: [
-            ]
-        };
-
-        const ideaId = await setIdea(idea);
-
-        await userCache[msg.author.id].ideas.push(ideaId);
-        await msg.reply(`A ideia **${ideaTitle}** foi adicionada a sua lista de ideias com o id **${ideaId}**, para debatê-la execute o comando **;discutir ${ideaId}**`);
+        await ideaList(msg, args)
+        await ideaAdd(msg, args)
     },
 };
--- bot/functions/ideas/ideas.js
+++ bot/functions/ideas/ideas.js
@@ -4,24 +4,27 @@ const { db } = require("../../data/firebase");
 let ideaCache = require("../../data/cache/ideas.json");
 
 async function getIdea(ideaId) {
+	if (ideaCache[ideaId]) return;
 
 	const ideaRef = doc(db, "ideas", ideaId);
 	const ideaSnap = await getDoc(ideaRef);
 
+	if (!ideaSnap.exists()) return
+
 	ideaCache[ideaId] = ideaSnap.data();
+	console.log(ideaCache[ideaId])
 }
 
-async function setIdea(idea) {
+async function addIdea(idea) {
 	const ideaColRef = collection(db, "ideas");
 	const ideaColSnap = await getCountFromServer(ideaColRef);
-	const ideaId = await ideaColSnap
+	const ideaId = ideaColSnap
 		.data()
 		.count
 		.toString();
 	const ideaRef = doc(db, 'ideas', ideaId)
-	console.log(ideaRef, idea)
 	await setDoc(ideaRef, idea);
-	return await ideaId
+	return ideaId
 }
 
-module.exports = { getIdea, setIdea };
+module.exports = { getIdea, addIdea };
