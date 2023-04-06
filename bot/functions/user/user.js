6d350b950a1de1a3df9d3444c1f65ff7cdf4db9d
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-05 21:05:39 -0300

    idk why i made this sorry

--- bot/functions/user/user.js
+++ bot/functions/user/user.js
@@ -2,16 +2,25 @@ const { doc, getDoc, setDoc } = require("firebase/firestore");
 
 const { db } = require("../../data/firebase");
 let userCache = require('../../data/cache/users.json');
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const c = new Client({
+    intents: [
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ],
+})
 
 async function getUser(userId) {
     if (userCache[userId]) return;
 
     const userRef = doc(db, 'users', userId);
     const userSnap = await getDoc(userRef);
-
     if (!userSnap.exists()) {
         const user = {
             userId: {
+                name: c.users.cache.get(userId).username,
                 money: 100,
                 ideas: []
             }
@@ -31,6 +40,7 @@ async function setUser(userId) {
 }
 
 function userUpdater(userId) {
+    if (!userCache[userId].name) userCache[userId].name = '';
     if (!userCache[userId].money) userCache[userId].money = 100;
     if (!userCache[userId].ideas) userCache[userId].ideas = [];
 
