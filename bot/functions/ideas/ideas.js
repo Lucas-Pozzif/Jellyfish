553a7252d601af27933de42c1f515f1d37eeec28
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-05 21:03:59 -0300

    getidea now returns true if it finds the idea

--- bot/functions/ideas/ideas.js
+++ bot/functions/ideas/ideas.js
@@ -4,14 +4,15 @@ const { db } = require("../../data/firebase");
 let ideaCache = require("../../data/cache/ideas.json");
 
 async function getIdea(ideaId) {
-	if (ideaCache[ideaId]) return;
+	if (ideaCache[ideaId]) return true;
 
 	const ideaRef = doc(db, "ideas", ideaId);
 	const ideaSnap = await getDoc(ideaRef);
 
-	if (!ideaSnap.exists()) return
+	if (!ideaSnap.exists()) return false
 
 	ideaCache[ideaId] = ideaSnap.data();
+	return true
 }
 
 async function addIdea(idea) {
@@ -32,4 +33,4 @@ async function setIdea(ideaId) {
 
 }
 
-module.exports = { getIdea, addIdea,setIdea };
+module.exports = { getIdea, addIdea, setIdea };
