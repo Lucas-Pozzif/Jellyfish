e11a1492c2a9f827d0b7eed1aadfabb37c637d75
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-10 20:11:38 -0300

    idea command cleanup

--- bot/commands/ideas/idea.js
+++ bot/commands/ideas/idea.js
@@ -22,19 +22,9 @@ module.exports = {
     async execute(msg, args) {
         idea.title = args.join(' ');
         idea.created.by = msg.author.id
-
         const ideaId = await addIdea(idea);
 
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('primary')
-                    .setLabel('Fodase!')
-                    .setStyle(ButtonStyle.Primary),
-            );
-
         userCache[msg.author.id].ideas.push(ideaId);
-        await msg.reply({ content: `Você deseja adicionar a ideia ${idea.title} à sua lista de ideias?`, components: [row] })
 
         await msg.reply(`A ideia **${idea.title}** foi adicionada a sua lista de ideias com o id **${ideaId}**, para debatê-la execute o comando **;discutir ${ideaId}**`);
     },
